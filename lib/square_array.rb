a1492ee7baa2a317d38555401ab75372767f8568
Chelsey <cswilik@gmail.com>
2020-10-09 15:27:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = [ ]
   counter = 0 
   while counter < array.length do 
-    array.index  << new_array
+    array.index ** 2 << new_array
     counter += 1 
   end 
   return new_array
